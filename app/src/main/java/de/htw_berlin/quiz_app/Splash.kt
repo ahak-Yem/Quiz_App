25fb038d6652afbc29cbbe92ed4d79ec02023dc6
Fantomas077 <nheltonn@gmail.com>
2023-06-29 14:52:52 +0200

    splash screen

---
+++ app/src/main/java/de/htw_berlin/quiz_app/Splash.kt
@@ -0,0 +1,15 @@
+package de.htw_berlin.quiz_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
