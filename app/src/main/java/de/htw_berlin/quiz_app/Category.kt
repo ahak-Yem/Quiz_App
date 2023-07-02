8608f84ee9cad4aedde285893fd43b4b4f8bc101
Ahmed Kutbi <Ahmed.Kutbi@Student.HTW-Berlin.de>
2023-07-02 04:22:43 +0200

    Added a Category data class that works as a data model for our data structure on firestore Added: Category.kt
    Added a Category DB class to handle the communication about the Category collection with our firestore instance fo.
    Added: CategoryDB.kt

---
+++ app/src/main/java/de/htw_berlin/quiz_app/Category.kt
@@ -0,0 +1,4 @@
+package de.htw_berlin.quiz_app
+
+//Data model that looks like the data structure of our Categories collection on firestore
+data class Category(val id:String,val name:String,val iconPath:String)
---
+++ app/src/main/java/de/htw_berlin/quiz_app/CategoryDB.kt
@@ -0,0 +1,44 @@
+package de.htw_berlin.quiz_app
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+//This class handles communication with DB to fetch Category Collection
+class CategoryDB {
+
+    /*TODO:Frag Herr Neumann:Does it make since to get the Firestore Instance whenever we want to
+       access it or to create it somewhere in a singleton class and save it in a variable to reuse
+        it for other collections?*/
+
+    //A private attribute field with datatype(FirebaseFirestore) that saves our firestore instance
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    //A function to get the data from firestore, saves it in a list and pass it
+    //using the onSuccess(list) function if failed then exception.
+    //We will pass the two function that are gonna be called in the parameter when we call this fun
+    fun getCategories(onSuccess: (List<Category>) -> Unit, onFailure: (Exception) -> Unit) {
+
+        //Name of our Collection on firestore
+        val categoriesCollection = firestore.collection("Categories")
+
+        //Trying to get all the documents
+        //If successful then we will play with the queryResult=collection
+        categoriesCollection.get().addOnSuccessListener { queryResult ->
+            val categoryList = mutableListOf<Category>()
+
+            //foreach document in our collection
+            for (document in queryResult.documents) {
+
+                //Get the document id and the 2 fields from the document.
+                val categoryId = document.id
+                val categoryName = document.getString("Name") ?: ""
+                val categoryIconUrl = document.getString("IconPath") ?: ""
+
+                //Save the document data as an object of the data class "Category" see Category.kt
+                categoryList.add(Category(categoryId, categoryName, categoryIconUrl))
+            }
+            onSuccess(categoryList)
+        }.addOnFailureListener { exception ->
+            onFailure(exception)
+        }
+    }
+}
