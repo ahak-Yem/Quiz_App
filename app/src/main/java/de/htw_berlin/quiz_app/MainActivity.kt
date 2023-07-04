fc05fffd2b1de52fb75558c879a05b4100099d3a
Fantomas077 <nheltonn@gmail.com>
2023-07-05 00:27:51 +0200

    Anmeldung daten von Users in Db speichern und implementierung von den Anmeldungsmöglichkeiten Fall 1: Spitzname -> existiert Name -> richtig =Einloggen
    Fall 2:
    Spitzname -> nicht existiert
    =registrieren
    
    Fall 3:
    Spitzname -> existiert
    Name -> falsch
    =Bitte wählen Sie einen anderen Spitznam ein.

--- app/src/main/java/de/htw_berlin/quiz_app/MainActivity.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/MainActivity.kt
@@ -1,5 +1,7 @@
 package de.htw_berlin.quiz_app
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +9,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this) // Initialize Firebase
+
         val start = findViewById<Button>(R.id.Start_btn)
         val name = findViewById<EditText>(R.id.Username)
         val spitzname = findViewById<EditText>(R.id.Spitzname)
@@ -21,30 +27,71 @@ class MainActivity : AppCompatActivity() {
             txterror.visibility = View.VISIBLE
             val txtusername = name.text.toString()
             val txtspitzname = spitzname.text.toString()
-            if (txtusername.trim().isEmpty()  ){
-                txterror.text = "Name  eingeben"
+            if (txtusername.trim().isEmpty()) {
+                txterror.text = "Name eingeben"
                 txterror.visibility = View.VISIBLE
             }
-            else
-                if (txtspitzname.trim().isEmpty()  ){
-                    txterror.text = "Spitzname  eingeben"
-                    txterror.visibility = View.VISIBLE
-                }
-                else
-                    if (txtspitzname.trim().isEmpty() && txtusername.trim().isEmpty() ){
-                        txterror.text = "Spitzname und Name eingeben"
-                        txterror.visibility = View.VISIBLE
-                    }
+            if(txtspitzname.trim().isEmpty() && txtusername.trim().isEmpty())
+            {
+                txterror.text = "Spitzname und Name eingeben"
+                txterror.visibility = View.VISIBLE
+            }
+            else if (txtspitzname.trim().isEmpty()) {
+                txterror.text = "Spitzname eingeben"
+                txterror.visibility = View.VISIBLE
+            }
+
 
 
             else {
-                name.setText("")
+                val db = FirebaseFirestore.getInstance() // Reference to Firebase Firestore database
 
-                val intent = Intent(this, Modusauswahl::class.java)
-                intent.putExtra("username", txtusername)
-                startActivity(intent)
+                db.collection("Users")
+                    .document(txtspitzname)
+                    .get()
+                    .addOnSuccessListener { documentSnapshot ->
+                        if (documentSnapshot.exists()) {
+                            val existingUsername = documentSnapshot.getString("Name")
+
+                            if (existingUsername == txtusername) {
+                                // Spitzname und Name sind identisch
+                                val intent = Intent(this, Modusauswahl::class.java)
+                                intent.putExtra("username", txtusername)
+                                startActivity(intent)
+                                spitzname.setText("")
+                                name.setText("")
+                            } else {
+                                // Spitzname existiert bereits, aber Name ist unterschiedlich
+                                txterror.text = "Falscher Name"
+                            }
+                        } else {
+                            // Spitzname existiert nicht
+                            val user = hashMapOf(
+                                "Name" to txtusername
+                            )
+
+                            db.collection("Users")
+                                .document(txtspitzname)
+                                .set(user)
+                                .addOnSuccessListener {
+                                    // Daten wurden erfolgreich hinzugefügt
+                                    val intent = Intent(this, Modusauswahl::class.java)
+                                    intent.putExtra("username", txtusername)
+                                    startActivity(intent)
+                                    spitzname.setText("")
+                                    name.setText("")
+                                }
+                                .addOnFailureListener { e ->
+                                    // Fehler beim Hinzufügen
+                                    txterror.text = "Fehler beim Hinzufügen in die Datenbank."
+                                }
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        // Fehler beim Überprüfen des Spitznamens
+                        txterror.text = "Fehler beim Überprüfen der Spitznamen-Existenz."
+                    }
             }
-
         }
     }
 }
--- app/src/main/java/de/htw_berlin/quiz_app/Modusauswahl.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/Modusauswahl.kt
@@ -4,14 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class Modusauswahl : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modusauswahl)
+        val help = findViewById<TextView>(R.id.hilfe)
         val tv_willkommen=findViewById<TextView>(R.id.willkommen)
         val username= intent.getStringExtra("username")
+        help.setOnClickListener {
+
+            Toast.makeText(this, "Normalmodus besteht aus Categorie von Fragen. " +
+                    "Spielmodus da wird direkt Frage gestelt und falls du eine falsch antwortet vierlierst du das Spiel:)",
+                Toast.LENGTH_LONG).show()
+        }
+
+
 
         tv_willkommen.text="Hey $username"
     }
