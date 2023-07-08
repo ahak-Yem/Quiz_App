2d94e9bb7f26690b33686ebbca0f78c9259cd285
ahakDot <129846041+ahakDot@users.noreply.github.com>
2023-07-08 05:59:06 +0200

    Update the db class with queries to fetch random questions added: DB.kt

--- app/src/main/java/de/htw_berlin/quiz_app/DB.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/DB.kt
@@ -1,16 +1,14 @@
 package de.htw_berlin.quiz_app
-
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.random.Random
 
-//This class handles communication with DB to fetch Category Collection
-class DB {
-
+//This class handles communication with DB to fetch the collections
+class DB () {
     //A private attribute field with datatype(FirebaseFirestore) that saves our firestore instance
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    //A function to get the data from firestore, saves it in a list and pass it
-    //using the onSuccess(list) function if failed then exception.
-    //We will pass the two function that are gonna be called in the parameter when we call this fun
+    //A function to get all categories from firestore, saves it in a list and pass it
+    //using the onSuccess(list) callback function if failed then callback exception function.
     fun getCategories(onSuccess: (List<Category>) -> Unit, onFailure: (Exception) -> Unit) {
 
         //Name of our Collection on firestore
@@ -35,6 +33,47 @@ class DB {
             onSuccess(categoryList)
         }.addOnFailureListener { exception ->
             onFailure(exception)
+        }
+     }
+
+    //This function fetches all questions from the db filter with category and choose random 10 questions from them
+    //Using the onSuccess(list) callback function we will pass the questions
+    fun getRandomQuestions(category: Category,selectCount:Int ,maxRetries: Int = 3, onSuccess: (List<Question>) -> Unit) {
+        //The quesy to fetches all questions from a category
+        val allQuestionsQuery = firestore.collection("Questions").whereEqualTo("category", category)
+        //Fetches the questions by applying the query
+        allQuestionsQuery.get()
+            .addOnSuccessListener { querySnapshot ->
+                val allQuestions: List<Question> = querySnapshot.documents.mapNotNull { documentSnapshot ->
+                    documentSnapshot.toObject(Question::class.java) //Saves all questions that are not null in a List<Question>
+                }
+                val randomQuestions:List<Question> = selectRandomQuestions(allQuestions, selectCount)//Select # questions from all
+                onSuccess(randomQuestions)//returns the fetched questions
+            }
+            .addOnFailureListener { exception ->
+                if (maxRetries > 0) {
+                    getRandomQuestions(category, selectCount,maxRetries - 1, onSuccess)//retries the process 3 times just in case
+                } else {
+                    // TODO: Handle the failure and provide an appropriate callback or error message
+                }
+            }
+    }
+
+    //This method selects 10 random questions
+    private fun selectRandomQuestions(questions: List<Question>, count: Int): List<Question> {
+        if (count < questions.size && count > 0) {
+            val randomIndices = mutableListOf<Int>()
+            while (randomIndices.size < count) {
+                val randomIndex = Random.nextInt(1, questions.size + 1)
+                val questionId = "Q$randomIndex"
+                val index = questions.indexOfFirst { it.id == questionId }
+                if (index != -1 && index !in randomIndices) {
+                    randomIndices.add(index)
+                }
+            }
+            return randomIndices.map { questions[it] }
+        } else {
+            return questions
         }
     }
 }
