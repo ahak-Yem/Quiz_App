b18eb73163dba4150aa2bc03f90b2bc87d361d94
Ahmed Kutbi <Ahmed.Kutbi@Student.HTW-Berlin.de>
2023-07-11 00:40:30 +0200

    Show icons in the item_category.xml

--- app/src/main/java/de/htw_berlin/quiz_app/CategoryItemViewHolder.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/CategoryItemViewHolder.kt
@@ -7,9 +7,24 @@ import de.htw_berlin.quiz_app.databinding.ItemCategoryBinding
 class CategoryItemViewHolder (private val binding:ItemCategoryBinding):
     RecyclerView.ViewHolder(binding.root){
 
+    //Every icon name in the DB should be mapped like this
+    val iconMapping = mapOf(
+        "geography_category_pictogram" to R.drawable.geography_category_pictogram,
+        "maths_category_pictogram" to R.drawable.maths_category_pictogram,
+        "music_category_pictogram" to R.drawable.music_category_pictogram,
+        "programming_category_pictogram" to R.drawable.programming_category_pictogram,
+        "sport_category_pictogram" to R.drawable.sport_category_pictogram
+    )
+
     //binds the category object to the category variable in the item_category layout
     fun bind(category: Category){
         binding.category=category
+        bindIcon(category)
         binding.executePendingBindings()
     }
+    fun bindIcon(category: Category) {
+        val iconResourceId = iconMapping[category.iconPath] ?: R.drawable.default_category
+        binding.categoryIcon.setImageResource(iconResourceId)
+    }
+
 }
