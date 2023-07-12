8de5c0ab2a9b689ef9846abe1c2d57cb8c980e29
Fantomas077 <nheltonn@gmail.com>
2023-07-12 10:05:22 +0200

    weiter mit spielmodus. timeout ,weiter Chekanswer

--- app/src/main/java/de/htw_berlin/quiz_app/Spielmodus.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/Spielmodus.kt
@@ -13,44 +13,60 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import android.os.CountDownTimer
+import android.widget.Toast
 
 class Spielmodus : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    private lateinit var fragennummerTextView: TextView
     private lateinit var fragenTextView: TextView
-    private lateinit var topicNameTextView: TextView
     private lateinit var option1Button: Button
     private lateinit var option2Button: Button
     private lateinit var option3Button: Button
     private lateinit var option4Button: Button
+    private lateinit var nextButton: Button
 
+    private var currentQuestionIndex: Int = 0
+    private val totalQuestions: Int = 10
+    private lateinit var questionsList: MutableList<String>
+    private lateinit var suggestionsMap: MutableMap<String, List<String>>
+    private lateinit var answersMap: MutableMap<String, String>
     private var currentQuestion: String = ""
     private var currentAnswer: String = ""
+    private var userAnswered: Boolean = false
+
+    private var timeRemaining: Long = 25000 // 25 Sekunden in Millisekunden
+    private lateinit var countdownTimer: CountDownTimer
+    private lateinit var timerTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_spielmodus, container, false)
 
+        fragennummerTextView = view.findViewById(R.id.fragennummer)
         fragenTextView = view.findViewById(R.id.fragen)
         option1Button = view.findViewById(R.id.option1)
         option2Button = view.findViewById(R.id.option2)
         option3Button = view.findViewById(R.id.option3)
         option4Button = view.findViewById(R.id.option4)
+        nextButton = view.findViewById(R.id.nextbtn)
+        nextButton.isEnabled = false
+        timerTextView = view.findViewById(R.id.timer)
 
-        // Initialisation de Firebase Firestore
+        // Initialisierung von Firebase Firestore
         val db = FirebaseFirestore.getInstance()
 
-        // Récupérer les questions depuis la collection "Questions" dans Firebase Firestore
+        // Fragen aus der Sammlung "Questions" in Firebase Firestore abrufen
         GlobalScope.launch(Dispatchers.Main) {
             try {
                 val questionsQuerySnapshot = db.collection("Questions").get().await()
-                val questionsList = mutableListOf<String>()
-                val suggestionsMap = mutableMapOf<String, List<String>>()
-                val answersMap = mutableMapOf<String, String>()
+                questionsList = mutableListOf<String>()
+                suggestionsMap = mutableMapOf<String, List<String>>()
+                answersMap = mutableMapOf<String, String>()
 
-                // Parcourir les documents de la collection "Questions"
+                // Durchlaufe die Dokumente in der Sammlung "Questions"
                 for (questionDocumentSnapshot: QueryDocumentSnapshot in questionsQuerySnapshot) {
                     val questionText = questionDocumentSnapshot.getString("Text")
                     val suggestions = questionDocumentSnapshot.get("Suggestions") as? List<String>
@@ -61,57 +77,136 @@ class Spielmodus : Fragment() {
                     answer?.let { answersMap[questionText!!] = it }
                 }
 
-                // Mélanger la liste de questions de manière aléatoire
+                // Mische die Liste der Fragen zufällig
                 questionsList.shuffle()
 
-                // Mettre à jour le TextView avec la première question (indice 0)
-                if (questionsList.isNotEmpty()) {
-                    currentQuestion = questionsList[0]
-                    currentAnswer = answersMap[currentQuestion] ?: ""
+                // Aktualisiere die Fragenummer
+                val questionNumberText = "${currentQuestionIndex + 1}/$totalQuestions"
+                fragennummerTextView.text = questionNumberText
 
-                    fragenTextView.text = currentQuestion
+                // Lade die erste Frage
+                loadQuestion()
 
-                    // Vérifier si les suggestions de réponses sont disponibles pour cette question
-                    if (suggestionsMap.containsKey(currentQuestion) && suggestionsMap[currentQuestion]?.size == 4) {
-                        val suggestions = suggestionsMap[currentQuestion]!!
+                // Füge Click Listener für die Optionsbuttons hinzu
+                option1Button.setOnClickListener { checkAnswer(option1Button.text.toString()) }
+                option2Button.setOnClickListener { checkAnswer(option2Button.text.toString()) }
+                option3Button.setOnClickListener { checkAnswer(option3Button.text.toString()) }
+                option4Button.setOnClickListener { checkAnswer(option4Button.text.toString()) }
 
-                        option1Button.text = suggestions[0]
-                        option2Button.text = suggestions[1]
-                        option3Button.text = suggestions[2]
-                        option4Button.text = suggestions[3]
-                    } else {
-                        // Cacher les boutons d'option si les suggestions ne sont pas disponibles
-                        option1Button.visibility = View.GONE
-                        option2Button.visibility = View.GONE
-                        option3Button.visibility = View.GONE
-                        option4Button.visibility = View.GONE
+                // Füge Click Listener für den "Weiter"-Button hinzu
+                nextButton.setOnClickListener { loadNextQuestion() }
+
+                // Timer konfigurieren
+                countdownTimer = object : CountDownTimer(timeRemaining, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        timeRemaining = millisUntilFinished
+                        val seconds = timeRemaining / 1000
+                        val formattedTime = String.format("%02d:%02d", seconds / 60, seconds % 60)
+                        timerTextView.text = formattedTime
+                    }
+
+                    override fun onFinish() {
+                        // Zeit ist abgelaufen, hier kannst du es als automatisch falsche Antwort behandeln oder etwas anderes tun
+                        // Rufe die Methode checkAnswer mit einer standardmäßig falschen Antwort auf
+                        checkAnswer("")
                     }
                 }
+                countdownTimer.start()
             } catch (e: Exception) {
-                // Gérer les erreurs lors de la récupération des questions depuis Firebase
+                // Fehlerbehandlung beim Abrufen der Fragen aus Firebase
             }
         }
-
-        // Ajouter un écouteur de clic pour les boutons d'option
-        option1Button.setOnClickListener { checkAnswer(option1Button.text.toString()) }
-        option2Button.setOnClickListener { checkAnswer(option2Button.text.toString()) }
-        option3Button.setOnClickListener { checkAnswer(option3Button.text.toString()) }
-        option4Button.setOnClickListener { checkAnswer(option4Button.text.toString()) }
 
         return view
     }
 
-    private fun checkAnswer(selectedOption: String) {
-        if (selectedOption == currentAnswer) {
-            // Réponse correcte
-            highlightButtonGreen(selectedOption)
+    private fun loadQuestion() {
+        currentQuestion = questionsList[currentQuestionIndex]
+        currentAnswer = answersMap[currentQuestion] ?: ""
+
+        fragenTextView.text = currentQuestion
+
+        // Überprüfe, ob Vorschläge für diese Frage verfügbar sind
+        if (suggestionsMap.containsKey(currentQuestion) && suggestionsMap[currentQuestion]?.size == 4) {
+            val suggestions = suggestionsMap[currentQuestion]!!
+
+            option1Button.text = suggestions[0]
+            option2Button.text = suggestions[1]
+            option3Button.text = suggestions[2]
+            option4Button.text = suggestions[3]
+
+            // Aktiviere die Optionsbuttons
+            option1Button.isEnabled = true
+            option2Button.isEnabled = true
+            option3Button.isEnabled = true
+            option4Button.isEnabled = true
+
+            // Setze den Hintergrund der Buttons zurück
+            resetButtonStyles()
         } else {
-            // Réponse incorrecte
+            // Verstecke die Optionsbuttons, wenn keine Vorschläge verfügbar sind
+            option1Button.visibility = View.GONE
+            option2Button.visibility = View.GONE
+            option3Button.visibility = View.GONE
+            option4Button.visibility = View.GONE
+        }
+    }
+    // ...
+
+    private fun loadNextQuestion() {
+        if (userAnswered) {
+            // Überprüfe, ob alle Fragen gestellt wurden
+            if (currentQuestionIndex < totalQuestions - 1) {
+                currentQuestionIndex++
+
+                // Aktualisiere die Fragenummer
+                val questionNumberText = "${currentQuestionIndex + 1}/$totalQuestions"
+                fragennummerTextView.text = questionNumberText
+
+                loadQuestion()
+
+                // Setze den Zustand der Benutzerantwort zurück
+                userAnswered = false
+                nextButton.isEnabled = false
+
+                // Setze den Timer zurück
+                countdownTimer.cancel()
+                countdownTimer.start()
+            } else {
+                // Alle Fragen wurden gestellt
+                // Hier kannst du es behandeln, wie du möchtest, z.B. eine Nachricht anzeigen oder den Quiz-Vorgang neu starten.
+                // Beispiel für eine Toast-Nachricht:
+                Toast.makeText(requireContext(), "Quiz abgeschlossen!", Toast.LENGTH_SHORT).show()
+
+                // Quiz-Vorgang neu starten:
+                currentQuestionIndex = 0
+                questionsList.shuffle()
+                loadQuestion()
+                userAnswered = false
+                nextButton.isEnabled = false
+                countdownTimer.cancel()
+                countdownTimer.start()
+            }
+        }
+    }
+
+    private fun checkAnswer(selectedOption: String) {
+        countdownTimer.cancel()
+        if (selectedOption == currentAnswer) {
+            //Koorekte antwort
+            highlightButtonGreen(selectedOption)
+            userAnswered = true
+            nextButton.isEnabled = true
+        }
+        else {
+            // nicht korrekte
             highlightButtonRed(selectedOption)
             highlightButtonGreen(currentAnswer)
+            userAnswered = true
+            nextButton.isEnabled = true
         }
 
-        // Désactiver les boutons d'option pour empêcher les clics supplémentaires
+        // 
         option1Button.isEnabled = false
         option2Button.isEnabled = false
         option3Button.isEnabled = false
@@ -135,4 +230,21 @@ class Spielmodus : Fragment() {
             option4Button.text -> option4Button.setBackgroundResource(R.drawable.round_back_red)
         }
     }
+
+    private fun resetButtonStyles() {
+        option1Button.setBackgroundResource(R.drawable.round_back_white)
+        option2Button.setBackgroundResource(R.drawable.round_back_white)
+        option3Button.setBackgroundResource(R.drawable.round_back_white)
+        option4Button.setBackgroundResource(R.drawable.round_back_white)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Timer stoppen, um Speicherlecks zu vermeiden, wenn das Fragment zerstört wird
+        countdownTimer.cancel()
+    }
 }
+
+
+
