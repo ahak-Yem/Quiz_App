f29b7d20507cce72a363ea3ab41d5b33e8bedf41
Fantomas077 <nheltonn@gmail.com>
2023-07-16 21:19:19 +0200

    winner ->user gewinnt das spiel erweiterung mit buttn

--- app/src/main/java/de/htw_berlin/quiz_app/Win.kt
+++ app/src/main/java/de/htw_berlin/quiz_app/Win.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class Win : Fragment() {
+    private var name: String? = null
     // TODO: Rename and change types of parameters
 
     override fun onCreateView(
@@ -15,7 +17,42 @@ class Win : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_win, container, false)
+        val bundle = arguments
+        name = arguments?.getString("name")
+        val view = inflater.inflate(R.layout.fragment_lost, container, false)
+        val tryAgainButton: Button = view.findViewById(R.id.tryagainbtn)
+        tryAgainButton.setOnClickListener {
+            Neuesession()
+        }
+
+        val zumModusAuswahlButton: Button = view.findViewById(R.id.zum_modus_auswahl)
+        zumModusAuswahlButton.setOnClickListener {
+            ZumModusAuswahl()
+        }
+
+        return view
+        return view
+    }
+    private fun Neuesession() {
+        val spielmodusFragment = Spielmodus()
+
+        val fragmentManager = requireActivity().supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, spielmodusFragment)
+            .commit()
+    }
+
+    private fun ZumModusAuswahl() {
+
+        val modusAuswahlFragment = ModusAuswahl()
+        val bundle = Bundle()
+        bundle.putString("name", name)
+        modusAuswahlFragment.arguments = bundle
+
+        val fragmentManager = requireActivity().supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, modusAuswahlFragment)
+            .commit()
     }
 
 
